cc594455b88331c2b6b65f8650e47a10e519afdc
Michael Miriti <m.s.miriti@gmail.com>
2021-06-05 18:39:43 +0300

    больше try/catch для "стабильности". хештеги к типам сообщений

--- src/bot.js
+++ src/bot.js
@@ -11,30 +11,34 @@ module.exports = class Bot {
    * @param {Array<String>} to Список каналов для отправки
    */
   async fetchShoutbox(to) {
-    let response = await fetch(
-      'http://igdc.ru/infusions/shoutbox_panel/shoutbox.php',
-    ).then(r => r.json());
+    try {
+      let response = await fetch(
+        'http://igdc.ru/infusions/shoutbox_panel/shoutbox.php',
+      ).then(r => r.json());
 
-    for (let msg of response['messages']) {
-      const exists = await db.get('chat', msg['id']);
+      for (let msg of response['messages']) {
+        const exists = await db.get('chat', msg['id']);
 
-      if (!exists) {
-        await db.store('chat', {
-          id: msg['id'],
-        });
+        if (!exists) {
+          await db.store('chat', {
+            id: msg['id'],
+          });
 
-        const chatMessage =
-          '<a href="http://igdc.ru/infusions/shoutbox_panel/shoutbox_archive.php">Мини-чат</a>' +
-          '\n' +
-          '<b>' +
-          msg['user']['name'].trim() +
-          '</b>: ' +
-          msg['text'];
+          const chatMessage =
+            '#миничат' +
+            '\n' +
+            '<b>' +
+            msg['user']['name'].trim() +
+            '</b>: ' +
+            msg['text'];
 
-        for (let chan of to) {
-          await this.api.sendMessage(chan, chatMessage);
+          for (let chan of to) {
+            await this.api.sendMessage(chan, chatMessage);
+          }
         }
       }
+    } catch (e) {
+      console.error(e);
     }
   }
 
@@ -44,27 +48,32 @@ module.exports = class Bot {
    * @param {Array<String>} to Список каналов для отправки
    */
   async fetchNews(to) {
-    const allNews = await news.getNews();
+    try {
+      const allNews = await news.getNews();
 
-    for (let newsItem of allNews) {
-      const exists = await db.get('news', newsItem['id']);
+      for (let newsItem of allNews) {
+        const exists = await db.get('news', newsItem['id']);
 
-      if (!exists) {
-        await db.store('news', { id: newsItem['id'] });
+        if (!exists) {
+          await db.store('news', { id: newsItem['id'] });
 
-        const channelMessage =
-          '<b>' +
-          newsItem['title'] +
-          '</b>\n' +
-          newsItem['html'] +
-          '\n\n<i>' +
-          newsItem['author'] +
-          '</i>';
+          const channelMessage =
+            '#новости\n' +
+            '<b>' +
+            newsItem['title'] +
+            '</b>\n' +
+            newsItem['html'] +
+            '\n\n<i>' +
+            newsItem['author'] +
+            '</i>';
 
-        for (let chan of to) {
-          await this.api.sendMessage(chan, channelMessage);
+          for (let chan of to) {
+            await this.api.sendMessage(chan, channelMessage);
+          }
         }
       }
+    } catch (e) {
+      console.log(e);
     }
   }
 
@@ -74,27 +83,32 @@ module.exports = class Bot {
    * @param {Array<String>} to Список каналов для отправки
    */
   async fetchForum(to) {
-    const lastPost = await forum.getLastMessage();
+    try {
+      const lastPost = await forum.getLastMessage();
 
-    const existing = await db.get('forum', lastPost['id']);
+      const existing = await db.get('forum', lastPost['id']);
 
-    if (!existing) {
-      db.store('forum', { id: lastPost['id'] });
+      if (!existing) {
+        db.store('forum', { id: lastPost['id'] });
 
-      const chatMessage =
-        '<a href="' +
-        lastPost['url'] +
-        '">' +
-        lastPost['thread'] +
-        '</a>\n<b>' +
-        lastPost['username'] +
-        ':' +
-        '</b>\n' +
-        lastPost['html'];
+        const chatMessage =
+          '#форум\n' +
+          '<a href="' +
+          lastPost['url'] +
+          '">' +
+          lastPost['thread'] +
+          '</a>\n<b>' +
+          lastPost['username'] +
+          ':' +
+          '</b>\n' +
+          lastPost['html'];
 
-      for (let chan of to) {
-        this.api.sendMessage(chan, chatMessage);
+        for (let chan of to) {
+          this.api.sendMessage(chan, chatMessage);
+        }
       }
+    } catch (e) {
+      console.error(e);
     }
   }
 
